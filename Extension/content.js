8854306a34563c72c2c3f820168fc13cd4b04548
Luminous Journey <77587691+Luminous-Journey@users.noreply.github.com>
2023-12-02 01:54:23 -0800

    Update content.js
    complete re-write

--- Extension/content.js
+++ Extension/content.js
@@ -1,24 +1,48 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "selectLink") {
-    var links = document.querySelectorAll("a[rel='Next']");
-    var oldlink;
-
-    if (links.length > 0) {
-      var selectedLink = links[0];
-      selectedLink.style.border = "2px solid red";
-      if (oldlink != selectedLink){
-        console.log("Link with rel='Next' selected!");
+console.log("running")
+function arrow(selectedLink) {
+    document.addEventListener("keydown", function (event) {
+      if (event.key === "ArrowRight") {
+        window.location.href = selectedLink.href
+        return true;
       }
-      oldlink = selectedLink
-      // Add event listener for right arrow key press
-      document.addEventListener("keydown", function (event) {
-        if (event.key === "ArrowRight") {
-          // Navigate to the selected link
-          window.location.href = selectedLink.href;
-        }
-      });
-    } else {
-      console.log("No link with rel='Next' found on the page.");
-    }
+    });
+  
+}
+
+function rel() {
+  console.log("Using rel markers");
+  var links = document.querySelectorAll("a[rel='Next']");
+
+  if (links.length > 0) {
+    var selectedLink = links[0]
+    console.log("Captured with rel")
+    selectedLink.style.border = "2px solid green";
+    return selectedLink
   }
-});
+}
+
+function href() {
+  var links = document.querySelectorAll("a")
+  var oldlink
+  var selectedLink = null
+  if (links.length > 0) {
+    console.log("Using href markers");
+    for (var i = 0; i < links.length; i++) {
+      if (links[i].innerText.toLowerCase() === "next") {
+        selectedLink = links[i];
+        selectedLink.style.border = "2px solid red"
+        if (oldlink != selectedLink) {
+          console.log("Link with href selected!")
+        }
+        oldlink = selectedLink;
+        break
+      }
+    }
+    arrow(selectedLink)
+  }
+  return selectedLink
+}
+
+if (href() === null){
+  rel
+}
