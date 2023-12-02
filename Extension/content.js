e68c4b4f9b2f0db9af3c949b00d4aa1d5deeec9f
Luminous Journey <77587691+Luminous-Journey@users.noreply.github.com>
2023-12-01 23:25:18 -0800

    Update content.js
    only print to console once per finding an element

--- Extension/content.js
+++ Extension/content.js
@@ -1,12 +1,15 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "selectLink") {
     var links = document.querySelectorAll("a[rel='Next']");
+    var oldlink;
 
     if (links.length > 0) {
       var selectedLink = links[0];
       selectedLink.style.border = "2px solid red";
-      console.log("Link with rel='Next' selected!");
-
+      if (oldlink != selectedLink){
+        console.log("Link with rel='Next' selected!");
+      }
+      oldlink = selectedLink
       // Add event listener for right arrow key press
       document.addEventListener("keydown", function (event) {
         if (event.key === "ArrowRight") {
